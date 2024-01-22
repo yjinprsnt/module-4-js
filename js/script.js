c0e819efaabf1780f55a3e83bd8c4e4d52e7eab9
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-22 23:00:15 +0200

    add 6 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1406,3 +1406,23 @@
 // console.log(calculateTotalPrice([12, 85, 37, 4]));
 // console.log(calculateTotalPrice([164, 48, 291]));
 // console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+//   // Change code below this line
+
+//   numbers.forEach(item => {
+//     if (item > value) {
+//       filteredNumbers.push(item);
+//     }
+//   });
+
+//   //   for (let i = 0; i < numbers.length; i += 1) {
+//   //     if (numbers[i] > value) {
+//   //       filteredNumbers.push(numbers[i]);
+//   //     }
+//   //   }
+
+//   // Change code above this line
+//   return filteredNumbers;
+// }
