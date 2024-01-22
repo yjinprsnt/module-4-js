0827f9e10938dce45cfdf9bc7ec0a567194478ef
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-22 22:48:15 +0200

    add 4 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1356,3 +1356,34 @@
 // makePizza('Ultracheese', function eatPizza(pizzaName) {
 //   console.log(`Eating pizza ${pizzaName}`);
 // });
+
+// const pizzaPalace = {
+//   pizzas: ['Ultracheese', 'Smoked', 'Four meats'],
+//   order(pizzaName, onSuccess, onError) {
+//     for (const pizza of this.pizzas) {
+//       if (this.pizzas.includes(pizzaName)) {
+//         return onSuccess(pizzaName);
+//       }
+//       return onError(
+//         `There is no pizza with a name ${pizzaName} in the assortment.`
+//       );
+//     }
+//   },
+// };
+// // Change code above this line
+
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+
+// // Callback for onError
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
+
+// // Method calls with callbacks
+// pizzaPalace.order('Smoked', makePizza, onOrderError);
+// pizzaPalace.order('Four meats', makePizza, onOrderError);
+// pizzaPalace.order('Big Mike', makePizza, onOrderError);
+// pizzaPalace.order('Vienna', makePizza, onOrderError);
