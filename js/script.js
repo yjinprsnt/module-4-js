0f169abe1a4f602604a6ac68df71e0712affc10e
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-22 21:25:17 +0200

    add 2 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1326,3 +1326,19 @@
 
 // const result = makePizza();
 // const pointer = makePizza;
+
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// // Chande code below this line
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// }
+
+// console.log(makeMessage('Royal Grand', makePizza));
+// console.log(makeMessage('Ultracheese', deliverPizza));
