fcc3a93b8663298e843c33b6aa41ae0509006670
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-23 21:12:47 +0200

    add 11 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1462,3 +1462,17 @@
 //   return totalPrice;
 // };
 // // Change code above this line
+
+// // Change code below this line
+// const filterArray = (numbers, value) => {
+//   const filteredNumbers = [];
+
+//   numbers.forEach(number => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+//   // Change code above this line
+//   return filteredNumbers;
+// };
