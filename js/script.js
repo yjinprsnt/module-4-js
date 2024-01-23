bec5f7111dc5dd3a12e9c11e886b31cd5ef21f70
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-23 21:14:33 +0200

    add 12 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1476,3 +1476,17 @@
 //   // Change code above this line
 //   return filteredNumbers;
 // };
+
+// // Change code below this line
+// const getCommonElements = (firstArray, secondArray) => {
+//   const commonElements = [];
+
+//   firstArray.forEach(element => {
+//     if (secondArray.includes(element)) {
+//       commonElements.push(element);
+//     }
+//   });
+
+//   // Change code above this line
+//   return commonElements;
+// };
