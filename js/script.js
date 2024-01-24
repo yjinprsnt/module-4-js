b0e0f5ea744b9eaa0f247907fc05af24a424ed0f
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-24 21:37:08 +0200

    add 15 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1513,3 +1513,26 @@
 // const planets = ['Earth', 'Mars', 'Venus', 'Jupiter'];
 // // Change code below this line
 // const planetsLengths = planets.map(planet => planet.length);
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+// ];
+// // Change code below this line
+
+// const titles = books.map(book => book.title);
