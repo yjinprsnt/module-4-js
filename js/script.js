3bf7cdc84a72ae9aae0c3f91556036939f3561f1
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-24 21:45:29 +0200

    add 16 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1536,3 +1536,24 @@
 // // Change code below this line
 
 // const titles = books.map(book => book.title);
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     genres: ['adventure', 'history'],
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     genres: ['fiction'],
+//   },
+//   {
+//     title: 'Redder Than Blood',
+//     author: 'Tanith Lee',
+//     genres: ['horror', 'mysticism'],
+//   },
+// ];
+// // Change code below this line
+
+// const genres = books.flatMap(book => book.genres);
