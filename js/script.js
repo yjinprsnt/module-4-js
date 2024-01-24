12dd792bf720a163511fab9b2e367bb50f323750
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-24 22:40:47 +0200

    add 19 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1569,3 +1569,9 @@
 //   return users.map(user => user.email);
 // };
 // // Change code above this line
+
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// // Change code below this line
+
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+// const oddNumbers = numbers.filter(number => number % 2 !== 0);
