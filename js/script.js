8e95a72e25d546800f3fb59b85debab064c1fd53
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-01-25 22:06:26 +0200

    add 21 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1599,29 +1599,29 @@
 //   (course, index, array) => array.indexOf(course) === index
 // );
 
-const books = [
-  {
-    title: 'The Last Kingdom',
-    author: 'Bernard Cornwell',
-    rating: 8.38,
-  },
-  {
-    title: 'Beside Still Waters',
-    author: 'Robert Sheckley',
-    rating: 8.51,
-  },
-  {
-    title: 'The Dream of a Ridiculous Man',
-    author: 'Fyodor Dostoevsky',
-    rating: 7.75,
-  },
-  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
-  { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
-];
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+// ];
 
-const MIN_RATING = 8;
-const AUTHOR = 'Bernard Cornwell';
-// Change code below this line
+// const MIN_RATING = 8;
+// const AUTHOR = 'Bernard Cornwell';
+// // Change code below this line
 
-const topRatedBooks = books;
-const booksByAuthor = books;
+// const topRatedBooks = books.filter(book => book.rating > MIN_RATING);
+// const booksByAuthor = books.filter(book => book.author === AUTHOR);
