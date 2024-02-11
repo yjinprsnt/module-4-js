33f038db4d29c00e60b87b4b212c5ff9b4d67854
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-02-11 16:52:31 +0200

    add 32 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1708,3 +1708,17 @@
 // // Change code below this line
 // const isEveryUserActive = users => users.every(user => user.isActive);
 // // Change code above this line
+
+// const firstArray = [26, 94, 36, 18];
+// const secondArray = [17, 61, 23];
+// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// // Change below this line
+
+// const anyElementInFirstIsEven = firstArray.some(item => item / 2 !== 0);
+// const anyElementInFirstIsOdd = firstArray.some(item => item / 2 === 0);
+
+// const anyElementInSecondIsEven = secondArray.some(item => item / 2 === 0);
+// const anyElementInSecondIsOdd = secondArray.some(item => item / 2 !== 0);
+
+// const anyElementInThirdIsEven = thirdArray.some(item => item / 2 !== 0);
+// const anyElementInThirdIsOdd = thirdArray.some(item => item / 2 !== 0);
