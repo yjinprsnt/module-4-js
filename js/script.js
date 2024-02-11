bbbbcec26859296c23289eadc36e472cf4f5f5a4
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-02-11 16:35:10 +0200

    add 30 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1690,3 +1690,17 @@
 // const getUserWithEmail = (users, email) =>
 //   users.find(user => user.email === email);
 // // Change code above this line
+
+// const firstArray = [26, 94, 36, 18];
+// const secondArray = [17, 61, 23];
+// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// // Change code below this line
+
+// const eachElementInFirstIsEven = firstArray.every(item => item / 2 !== 0);
+// const eachElementInFirstIsOdd = firstArray.every(item => item / 2 === 0);
+
+// const eachElementInSecondIsEven = secondArray.every(item => item / 2 === 0);
+// const eachElementInSecondIsOdd = secondArray.every(item => item / 2 !== 0);
+
+// const eachElementInThirdIsEven = thirdArray.every(item => item / 2 === 0);
+// const eachElementInThirdIsOdd = thirdArray.every(item => item / 2 === 0);
