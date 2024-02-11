0aa05a4052932eb18b53bad424470ae2bcc3369b
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-02-11 19:31:43 +0200

    add 34 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1726,3 +1726,17 @@
 // // Change code below this line
 // const isAnyUserActive = users => users.some(user => user.isActive);
 // // Change code above this line
+
+// const players = {
+//   mango: 1270,
+//   poly: 468,
+//   ajax: 710,
+//   kiwi: 244,
+// };
+// const playtimes = Object.values(players); // [1270, 468, 710, 244]
+// // Change code below this line
+
+// const totalPlayTime = playtimes.reduce((value, time) => value + time);
+
+// // Change code above this line
+// const averagePlayTime = totalPlayTime / playtimes.length;
