b4aa3219b45633d2adf0580a7aa2973ede4edadf
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-02-11 15:10:23 +0200

    add 28 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1660,3 +1660,28 @@
 // // Change code below this line
 // const getInactiveUsers = users => users.filter(user => !user.isActive);
 // // Change code above this line
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+// ];
+// const BOOK_TITLE = 'The Dream of a Ridiculous Man';
+// const AUTHOR = 'Robert Sheckley';
+// // Change code below this line
+
+// const bookWithTitle = books.find(book => book.title === BOOK_TITLE);
+// const bookByAuthor = books.find(book => book.author === AUTHOR);
