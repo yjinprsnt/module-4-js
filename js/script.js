151df08d36fc0433cf7c2e4f5a17b561e32ff401
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-02-12 21:11:15 +0200

    add 39 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1776,3 +1776,10 @@
 // const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
 
 // const alphabeticalAuthors = [...authors].sort((a, b) => a.localeCompare(b));
+
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// // Change code below this line
+
+// const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
+
+// const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
