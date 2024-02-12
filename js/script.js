12ea295cb06162ba65db519367a05149b2c9e3d0
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-02-12 20:56:49 +0200

    add 37 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1759,3 +1759,8 @@
 // const calculateTotalBalance = users =>
 //   users.reduce((total, user) => total + user.balance, 0);
 
+// // Change code below this line
+// const getTotalFriendCount = users =>
+//   users.reduce((total, user) => total + user.friends.length, 0);
+// // Change code above this line
+
