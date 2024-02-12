ff43cde371ee5458f3fe5eaa898b31cb94bbdfad
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-02-12 21:05:12 +0200

    add 38 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1764,3 +1764,15 @@
 //   users.reduce((total, user) => total + user.friends.length, 0);
 // // Change code above this line
 
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const authors = [
+//   'Tanith Lee',
+//   'Bernard Cornwell',
+//   'Robert Sheckley',
+//   'Fyodor Dostoevsky',
+// ];
+// // Change code below this line
+
+// const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
+
+// const alphabeticalAuthors = [...authors].sort((a, b) => a.localeCompare(b));
