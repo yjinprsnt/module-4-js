88a830a87778f3be2bac4488eb3480f1686e44c9
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2024-02-13 20:33:02 +0200

    add 45 auto check js module-4

--- js/script.js
+++ js/script.js
@@ -1849,3 +1849,36 @@
 // const sortByName = users =>
 //   [...users].sort((a, b) => a.name.localeCompare(b.name));
 // // Change code above this line
+
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+//   {
+//     title: 'The Dreams in the Witch House',
+//     author: 'Howard Lovecraft',
+//     rating: 8.67,
+//   },
+// ];
+// const MIN_BOOK_RATING = 8;
+// // Change code below this line
+
+// const names = books
+//   .filter(book => book.rating > MIN_BOOK_RATING)
+//   .map(book => book.author)
+//   .sort((firstName, secondName) => firstName.localeCompare(secondName));
+
+// console.log(names);
